6d66fe95a215506d69e328bdd1ed8fcba6324478
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-01 22:58:23 -0400

    cargo fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
 use zero2prod::run;
 
 // Procedural macro which initializes an async runtime that block on (drives) HttpServer::run
