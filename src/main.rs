8738562b6520e5924c373c6e4b5282da32e49f26
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-02 23:35:05 -0400

    Modify the entry point of the binary to generate a listener bound to port 8000

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use zero2prod::run;
-
 // Procedural macro which initializes an async runtime that block on (drives) HttpServer::run
 // returned futures to completion.
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    let listener =
+        std::net::TcpListener::bind(("127.0.0.1", 8000)).expect("Failed to bind pot 8000.");
+    // Bubble up the io::Error  if we failed to bind the address
+    // Otherwise call .await on the Server
+    zero2prod::run(listener)?.await
 }
