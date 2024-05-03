31fa1bbd48be674f5e7c1fbde882ebe0858477a4
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-02 23:33:33 -0400

    modify lib's run function to accept a listener so that testing can be done from a random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+pub fn run(listener: std::net::TcpListener) -> Result<Server, std::io::Error> {
+    dbg!(&listener);
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
 }
