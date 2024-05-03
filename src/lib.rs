7fddf79162f96b4c0c5e5d847722393a692b5a2e
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-03 13:16:05 -0400

    Move `health_check` and `subscriptions` routes to their own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    name: String,
-    email: String,
-}
-
-async fn subscribe(web::Form(_form): web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
 pub mod routes;
 pub mod startup;
