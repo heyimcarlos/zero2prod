73a40642381186e52c5a7409559a24cf0119a7ac
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-02 23:34:06 -0400

    Modify health_check to use port 0 which scans for a free port at the OS level

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,30 @@
-// use zero2prod::run;
+#[tokio::test]
+async fn health_check_works() -> () {
+    // Arrange
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
 
-#[test]
-fn dummy_test() {
-    // run().await;
+    // Act
+    let response = client
+        .get(format!("{}/health_check", &addr))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    // Port 0 is special-cased at the OS level, when trying to bind port 0
+    // a scan will be triggered to find an available port, and the bind to it.
+    let listener =
+        std::net::TcpListener::bind(("127.0.0.1", 0)).expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
