2afab44ca9c34a9efb6279447430b1971a51c438
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-04 19:50:11 -0400

    the libs (app) entry point now received a postgres connection which uses an atomic reference counted (arc) to make the connection cloneable, which is necessary since actix_web spawns a worker for each core avaiable in our system.

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,18 @@
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes;
 
-pub fn run(listener: std::net::TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: std::net::TcpListener,
+    connection: PgConnection,
+) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
