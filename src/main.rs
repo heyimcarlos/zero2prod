0c9947001e6d334cc06001e2ed1700cfa237a300
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-07 12:33:14 -0400

    configure tracing in the binary entry point

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use env_logger::{Builder, Env};
 use sqlx::PgPool;
-use zero2prod::configuration::get_configuration;
+use zero2prod::{
+    configuration::get_configuration,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 // Procedural macro which initializes an async runtime that block on (drives) HttpServer::run
 // returned futures to completion.
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // init() calls `set_logger`. Fallback to printing all logs at info-level or above
-    // if RUST_LOG env variable has not been set.
-    Builder::from_env(Env::default().default_filter_or("info")).init();
+    //  NOTE: Configure tracing
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     // We use a connection pool, becase it allows for concurrent behavior. A pool contains multiple
     // connections, so when a query is to be executed, sqlx will borrow a connection from the pool,
@@ -20,5 +23,6 @@ async fn main() -> Result<(), std::io::Error> {
         .unwrap_or_else(|_| panic!("Failed to bind port {}", &configuration.application_port));
     // Bubble up the io::Error  if we failed to bind the address
     // Otherwise call .await on the Server
-    zero2prod::startup::run(listener, connection_pool)?.await
+    zero2prod::startup::run(listener, connection_pool)?.await?;
+    Ok(())
 }
