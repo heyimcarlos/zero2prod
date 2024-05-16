62bf958ee604e0b8652d373a915fadb6781a015d
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-16 12:45:30 -0400

    add `make_status_not_null_in_subscriptions` migration file

---
+++ migrations/20240516163924_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,11 @@
+-- Add migration script here
+-- Wrap the entire query in a transaction, just in case any part of the query fails
+-- then the parts that succeeded will be rollbacked.
+BEGIN;
+    -- Backfill `status` for historical records
+    UPDATE subscriptions
+        SET status = 'confirmed'
+        WHERE status IS NULL;
+    -- Make `status` mandatory
+    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
+COMMIT;
