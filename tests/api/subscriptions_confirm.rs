f80a17061134e63bac3a6ee281ba6b3770b1e503
Carlos De la Cruz <me@heyimcarlos.dev>
2024-05-18 13:42:22 -0400

    add tests that clicks on the confirmation email and checks the database to see if the user was confirmed

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -49,4 +49,39 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
 }
 
 #[tokio::test]
-async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {}
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    // clicking on the confirmation link sends a GET to `/subscriptions/confirm` which
+    // should confirm a subscriber if the subscription token is available.
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=carlos%20jose&email=carlos.cruz%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.to_owned()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
+    // Click the link
+    reqwest::get(confirmation_links.html)
+        .await
+        .expect("Failed to execute request to `/subscriptions/confirm`")
+        .error_for_status()
+        .unwrap();
+
+    // Assert
+    let saved = sqlx::query!("SELECT name, email, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to read the subscriptions table");
+
+    assert_eq!(saved.name, "carlos");
+    assert_eq!(saved.email, "carlos.cruz@gmail.com");
+    assert_eq!(saved.status, "confirmed");
+}
